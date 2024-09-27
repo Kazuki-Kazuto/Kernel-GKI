1403a5b67961b173f5321f31b3300621cf3f2225
Kaze <97781670+Kazuki-Kazuto@users.noreply.github.com>
2024-09-27 08:07:56 +0700

    Update service.sh
    Add tweaks,edit path

--- service.sh
+++ service.sh
@@ -72,3 +72,17 @@ resetprop -n debug.sf.early_gl_phase_offset_ns 3000000
 resetprop -n debug.sf.early_phase_offset_ns 500000
 resetprop -n debug.sf.high_fps_early_gl_phase_offset_ns 650000
 ro.tran_low_battery_60hz_refresh_rate.support=0
+write /sys/devices/system/cpu/cpu0/cpufreq/scaling_governor performance
+write /sys/devices/system/cpu/cpu1/cpufreq/scaling_governor performance
+write /sys/devices/system/cpu/cpu2/cpufreq/scaling_governor performance
+write /sys/devices/system/cpu/cpu3/cpufreq/scaling_governor performance
+write /sys/devices/system/cpu/cpufreq/policy0/scaling_governor performance
+write /sys/devices/system/cpu/cpufreq/policy4/scaling_governor performance
+write /sys/devices/system/cpu/cpufreq/performance/above_hispeed_delay 0
+write /sys/devices/system/cpu/cpufreq/performance/boost 1
+write /sys/devices/system/cpu/cpufreq/performance/go_hispeed_load 75
+write /sys/devices/system/cpu/cpufreq/performance/max_freq_hysteresis 1
+write /sys/devices/system/cpu/cpufreq/performance/align_windows 1
+write /sys/module/adreno_idler/parameters/adreno_idler_active 0
+write /sys/module/lazyplug/parameters/nr_possible_cores 8
+write /sys/module/msm_performance/parameters/touchboost 1
